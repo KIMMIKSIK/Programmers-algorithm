a45302b5ecb3c008c0e7b91ee43d2f8532562d06
KIMMINSIK <we_in@naver.com>
2022-04-23 22:23:29 +0900

    ok

---
+++ lv1 로또의 최고순위와 최저순위.js
@@ -0,0 +1,71 @@
+//나의풀이(에러처리포함)
+function solution(lottos, win_nums) {
+  try {
+    if (
+      lottos.filter((item) => item >= 0 && item <= 45).length < 6 ||
+      win_nums.filter((item) => item > 0 && item <= 45).length < 6
+    ) {
+      throw new Error("lottos는 0~45, win_nums는 1~45값을 입력하세요");
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+  let zeroCount = lottos.filter((s) => s == 0).length;
+  let rankObject = {};
+  var answer = [];
+  let test = [];
+  for (let i = 0; i < 6; i++) {
+    test.push(Number(lottos.filter((item) => item === win_nums[i]).join()));
+  }
+  rankObject = {
+    0: test.filter((item) => item != "").length + zeroCount,
+    1: test.filter((item) => item != "").length,
+  };
+  for (let i = 0; i < 2; i++) {
+    switch (rankObject[i]) {
+      case 2:
+        answer.push(5);
+        break;
+      case 3:
+        answer.push(4);
+        break;
+      case 4:
+        answer.push(3);
+        break;
+      case 5:
+        answer.push(2);
+        break;
+      case 6:
+        answer.push(1);
+        break;
+      default:
+        answer.push(6);
+        break;
+    }
+  }
+  return answer;
+}
+
+//다른 사람 풀이
+function solution(lottos, win_nums) {
+  const rank = [6, 6, 5, 4, 3, 2, 1];
+
+  let minCount = lottos.filter((v) => win_nums.includes(v)).length;
+  let zeroCount = lottos.filter((v) => !v).length;
+
+  const maxCount = minCount + zeroCount;
+
+  return [rank[maxCount], rank[minCount]];
+}
+
+//다른 사람 풀이
+function solution(lottos, win_nums) {
+  const answer = [];
+  const min = lottos.filter((n) => win_nums.includes(n)).length;
+  const max = lottos.filter((n) => n === 0).length + min;
+
+  max > 1 ? answer.push(7 - max) : answer.push(6);
+  min > 1 ? answer.push(7 - min) : answer.push(6);
+
+  return answer;
+}
